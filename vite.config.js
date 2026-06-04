d45937f1039cf1244d54f7cd17bfefbb23633e6c
Vedant Daga <vedantdaga04@gmail.com>
2026-06-03 22:34:05 -0400

    updated files for gh-pages hosting

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/My-Portfolio/',
+  base: 'https://github.com/DagaVedant/My-Portfolio',
   plugins: [react()],
   resolve: {
     alias: {
