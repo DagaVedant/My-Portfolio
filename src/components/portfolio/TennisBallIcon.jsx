bc4eb3321e8d09a2a51664b6bd09f3d56a7132d2
Vedant Daga <vedantdaga04@gmail.com>
2026-07-08 15:39:21 -0400

    reworked tennis ball

--- src/components/portfolio/TennisBallIcon.jsx
+++ src/components/portfolio/TennisBallIcon.jsx
@@ -1,9 +1,26 @@
-const TennisBallIcon = (props) => (
-  <svg {...props} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="12" cy="12" r="10" fill="#c8e619" />
-    <path d="M9 3C5.5 5.5 4 8.5 4 12C4 15.5 5.5 18.5 9 21" stroke="white" strokeWidth="1.5" strokeLinecap="round" />
-    <path d="M15 3C18.5 5.5 20 8.5 20 12C20 15.5 18.5 18.5 15 21" stroke="white" strokeWidth="1.5" strokeLinecap="round" />
-  </svg>
-);
+/** @param {import('react').SVGProps<SVGSVGElement>} props */
+const TennisBallIcon = (props) => {
+  return (
+    <svg {...props} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="12" cy="12" r="10" fill="#CDEB2F" />
+
+      <path
+        d="M9 3C5.5 5.5 4 8.5 4 12s1.5 6.5 5 9"
+        fill="none"
+        stroke="#fff"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+
+      <path
+        d="M15 3c3.5 2.5 5 5.5 5 9s-1.5 6.5-5 9"
+        fill="none"
+        stroke="#fff"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
 
 export default TennisBallIcon;
